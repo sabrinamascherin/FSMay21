4d4b8f68ae4a74823ae967350e0d16b166f06b99
sabrinamascherin <85136154+sabrinamascherin@users.noreply.github.com>
2022-01-08 17:57:23 -0500

    Week 10 Commit

--- config.js
+++ config.js
@@ -1,21 +1,9 @@
-const environment = process.env.NODE_ENV || 'development';
-
-let database = 'robogarden_app';
+let database = 'expense_app';
 let username = 'root';
 let password = '145!1002MaP$';
 let host = 'localhost';
 
-if(environment === 'production'){
-    database = 'hnte0dhp9ewsypht';
-    username = 'af8dz0ev035kditj';
-    password = 'dv35vwjjj4zve6m5';
-    host = 'ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-}
-
-const Sequelize = require('sequelize');
-const config = new Sequelize(database, username, password, {
-    host: 'ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    dialect: 'mysql'
-});
+const Sequelize = require ('sequelize');
+const config = new Sequelize("expense_app", "root", "145!1002MaP$", {dialect: 'mysql'});
 
 module.exports = config;
--- index.js
+++ index.js
@@ -1,11 +1,102 @@
 const express = require('express');
 const app = express();
+const config = require('./config');
+const cors = require('cors');
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const Budget = require('./models/budget');
+const Transaction = require('./models/transaction');
+const Goal = require('./models/goal');
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+
+app.use(cors());
+app.use(express.json());
+
+//database connection
+config.authenticate().then(() => {
+    console.log('Database connected');
+}).catch((err) => {
+    console.log(err);
 });
 
-//environment variable
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log('Server running on port 3000...');
+//user registration
+app.post('/register', function(req, res){
+    let plainPassword = req.body.password;
+
+    bcrypt.hash(plainPassword, saltRounds, function(err, hash){
+        let user_data = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: hash
+        };
+
+        User.create(user_data).then((result)=>{
+            res.status(200).send(result);
+        }).catch((err)=>{
+            res.status(500).send(err);
+        });
+    });
+});
+
+//user login
+app.post('/login', function(req, res){
+
+    let email = req.body.email;
+    let password = req.body.password;
+    let user_data = {
+        where: {email}
+    }
+
+    //Find a user that corresponds to the email
+    User.findOne(user_data).then((result) => {
+        if (result){
+            console.log(result);
+            bcrypt.compare(password, result.password, function(err, output){
+                console.log(output);
+                if(output){
+                    res.status(200).send(result);
+                }else{
+                    res.status(400).send('Incorrect password.');
+                }
+            });
+        }
+        else{
+            res.status(404).send('User does not exist.');
+        }
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+})
+
+//GET budget
+app.get('/', function(req,res){
+    Budget.findAll().then((result) => {
+        res.status(200).send(result);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+});
+
+//GET goal
+app.get('/', function(req,res){
+    Goal.findAll().then((result) => {
+        res.status(200).send(result);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+});
+
+//GET transactions
+app.get('/', function(req,res){
+    Transaction.findAll().then((result) => {
+        res.status(200).send(result);
+    }).catch((err) => {
+        res.status(500).send(err);
+    });
+});
+
+app.listen(process.env.PORT || 1000, () => {
+    console.log('Server running on port 1000...');
 });
